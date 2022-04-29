3118e2754aa6d5552b53676927875c1e5b5670ed
Taekyun Kim <brandon1609@naver.com>
2022-04-29 18:40:11 +0900

    Initial Commit

---
+++ flexible_pomodoro/flexible_pomodoroApp.swift
@@ -0,0 +1,20 @@
+//
+//  flexible_pomodoroApp.swift
+//  flexible_pomodoro
+//
+//  Created by taekkim on 2022/04/29.
+//
+
+import SwiftUI
+
+@main
+struct flexible_pomodoroApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
